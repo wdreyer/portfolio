0be73cc6213ea8dfbe688331c587aaa842f63403
Your name <dreyer.wil@gmail.com>
2023-12-26 12:31:50 +0100

    analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,14 @@ function App({ Component, pageProps }) {
       <Head>
         <title>William Dreyer Dev JS/React</title>
       </Head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NFRYWZPG1F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NFRYWZPG1F');
+</script>
       <Component {...pageProps} />
       </I18nextProvider>
   );
